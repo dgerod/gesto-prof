a265a198bbefb1ca8b90ffac83f2216c80a18ef0
Diego Escudero <dgerod@xyz-lab.org.es>
2022-01-23 15:42:12 +0100

    Create operations to calculate taxes form  130.

--- scripts/operations/__init__.py
+++ scripts/operations/__init__.py
@@ -3,3 +3,4 @@ from scripts.operations.add_incomes import *
 from scripts.operations.generate_books import *
 from scripts.operations.generate_invoices import *
 from scripts.operations.report_periods import *
+from scripts.operations.generate_m130 import *
--- scripts/operations/generate_m130.py
+++ scripts/operations/generate_m130.py
@@ -1,90 +1,123 @@
 # Modelo 130 de Agencia Tributaria de España
 
-incomes_without_taxes = 1146.45
-expenses_without_taxes = 2164.73
-withholding_irpf_previous_periods = 0. # No retenemos IRPF al ser profesor particular
+import os
+from scripts.common.configuration import Configuration
 
-payed_previous_period = 20.28
-negative_results_previous_periods = 296.07
+#    incomes_without_taxes = 1146.45
+#    expenses_without_taxes = 2164.73
+#    payed_previous_period = 20.28
+#    negative_results_previous_periods = 296.07
 
-# P1. Estimación directa
-# ------------------------
+def generate_m130(file_name):
 
-C01 = incomes_without_taxes # ingresos
-C02 = expenses_without_taxes # gastos
-C03 = C01 - C02 # rendimiento neto
+    configuration = Configuration()    
+    file_path = os.path.join(configuration.get_inputs_directory(),
+                             file_name)
+                             
+    import yaml
+    with open(file_path, 'r') as stream:
+        try:
+            data = yaml.safe_load(stream)
+            incomes_without_taxes = data['ingresos']
+            expenses_without_taxes = data['gastos']
+            payed_previous_period = data['pagado_anteriormente']
+            negative_results_previous_periods = data['resultados_negativos']
 
-if C03 > 0.:
-    C04 = 0.2* C03
-else:
-    C04 = 0.
+        except yaml.YAMLError as ex:
+            raise
 
-# Esta casilla es donde informaremos de los importes que ya hayamos ingresado en
-# los anteriores modelos presentados.
-C05 = payed_previous_period
+    output_file = os.path.join(configuration.get_outputs_directory(), "calculo_modelo_130.txt")
 
-# La casilla 06 corresponde a “Retenciones e ingresos a cuenta soportados por
-# las actividades incluidas en este apartado y correspondientes al período
-# comprendido entre el primer día del año y el último día del trimestre”. Es decir,
-# si eres profesional, en tus facturas aplicas retención. El importe total de
-# todas estas retenciones de las facturas emitidas desde el primer día del año hasta
-# el trimestre en que estás operando es lo que deberás poner.
-C06 = withholding_irpf_previous_periods
+    _generate_m130(incomes_without_taxes, expenses_without_taxes,
+                 payed_previous_period, negative_results_previous_periods, output_file)
 
-C07 = C04 - C05 - C06
 
-# P2. Actividades agrarias y demás
-# ------------------------
-# Todos las casillas a 0 ya que no es aplicable a un profesor particular
+def _generate_m130(incomes_without_taxes, expenses_without_taxes,
+                 payed_previous_period, negative_results_previous_periods, output_file):
 
-C11 = C07
+    withholding_irpf_previous_periods = 0. # No retenemos IRPF al ser profesor particular
 
-# P3. Liquidacion total
-# ------------------------
+    # P1. Estimación directa
+    # ------------------------
 
-if C11 > 0.:
-    C12 = C11
-else:
-    C12 = 0.
+    C01 = incomes_without_taxes # ingresos
+    C02 = expenses_without_taxes # gastos
+    C03 = C01 - C02 # rendimiento neto
 
-C13 = 0. # No aplicable
+    if C03 > 0.:
+        C04 = 0.2* C03
+    else:
+        C04 = 0.
 
-C14 = C12 - C13
-C15 = negative_results_previous_periods
-C16 = 0. # Ninguna deducción por vivienda habitual
-C17 = C14 - C15 - C16
+    # Esta casilla es donde informaremos de los importes que ya hayamos ingresado en
+    # los anteriores modelos presentados.
+    C05 = payed_previous_period
 
-C18 = 0. # Es cero al no ser una liquidación complementaria
-C19 = C17 - C18
+    # La casilla 06 corresponde a “Retenciones e ingresos a cuenta soportados por
+    # las actividades incluidas en este apartado y correspondientes al período
+    # comprendido entre el primer día del año y el último día del trimestre”. Es decir,
+    # si eres profesional, en tus facturas aplicas retención. El importe total de
+    # todas estas retenciones de las facturas emitidas desde el primer día del año hasta
+    # el trimestre en que estás operando es lo que deberás poner.
+    C06 = withholding_irpf_previous_periods
 
-print("I. Actividades económicas en estimación directa...")
-print("----------------------------------------------------------")
-print("   - Ingresos computables (01): ", C01)
-print("   - Gastos deducibles (02): ", C02)
-print("   - Rendimiento neto (03): ", C03)
-print("   - 20% del rendimiento neto (04): ", C04)
-print("   - A deducir de  los periodos anteriores:")
-print("      - Pagado (05): ", C05)
-print("      - Retenciones de IRPF en las facturas (06): ", withholding_irpf_previous_periods)
-print("   - Pago fraccionado previo del trimestre (07): ", C07)
-print("")
+    C07 = C04 - C05 - C06
 
-print("II. Actividades agrícolas, ganaderas, ...")
-print("----------------------------------------------------------")
-print("   Todas las casilla a 0 ya que no es aplicable")
-print("   - Pago fraccionad previo del trimestre (11):", C07)
-print("")
+    # P2. Actividades agrarias y demás
+    # ------------------------
+    # Todos las casillas a 0 ya que no es aplicable a un profesor particular
 
-print("III. Total liquidación.")
-print("----------------------------------------------------------")
-print("   - Suma de pagos fraccionados del primer trimestre (12):", C12)
-print("   - A deducir: Minoración por aplicación... (13):", C13)
-print("   - Diferencia (14):", C14)
-print("   - A deducir de  los periodos anteriores:")
-print("      - Resultados negativos de los periodos anteriores (15):", C15)
-print("      - Adquisicón o rehabilitación de vivienda habitual (16):", C16)
-print("   - Total (17):", C17)
-print("   - A deducir (autoliquidación complementaria)")
-print("      - Resultado a ingresar anteriores autoliquidaciones (18) :", C18)
-print("   - Resultado de la autoliquidación (19):", C19)
-print("")
+    C11 = C07
+
+    # P3. Liquidacion total
+    # ------------------------
+
+    if C11 > 0.:
+        C12 = C11
+    else:
+        C12 = 0.
+
+    C13 = 0. # No aplicable
+
+    C14 = C12 - C13
+    C15 = negative_results_previous_periods
+    C16 = 0. # Ninguna deducción por vivienda habitual
+    C17 = C14 - C15 - C16
+
+    C18 = 0. # Es cero al no ser una liquidación complementaria
+    C19 = C17 - C18
+
+    with open(output_file, "w") as f:
+
+        f.write("")
+        f.write("I. Actividades económicas en estimación directa...\n")
+        f.write("----------------------------------------------------------\n")
+        f.write("   - Ingresos computables (01): %f\n" % C01)
+        f.write("   - Gastos deducibles (02): %f\n" % C02)
+        f.write("   - Rendimiento neto (03): %f\n" % C03)
+        f.write("   - 20 porciento del rendimiento neto (04): %f\n" % C04)
+        f.write("   - A deducir de  los periodos anteriores:\n")
+        f.write("      - Pagado (05): %f\n" % C05)
+        f.write("      - Retenciones de IRPF en las facturas (06): %f\n" % withholding_irpf_previous_periods)
+        f.write("   - Pago fraccionado previo del trimestre (07): %f\n" % C07)
+        f.write("\n")
+
+        f.write("II. Actividades agrícolas, ganaderas, ...\n")
+        f.write("----------------------------------------------------------\n")
+        f.write("   Todas las casilla a 0 ya que no es aplicable\n")
+        f.write("   - Pago fraccionado previo del trimestre (11): %f\n" % C07)
+        f.write("\n")
+
+        f.write("III. Total liquidación.\n")
+        f.write("----------------------------------------------------------\n")
+        f.write("   - Suma de pagos fraccionados del primer trimestre (12): %f\n" % C12)
+        f.write("   - A deducir: Minoración por aplicación... (13): %f\n" % C13)
+        f.write("   - Diferencia (14): %f\n" % C14)
+        f.write("   - A deducir de  los periodos anteriores:\n")
+        f.write("      - Resultados negativos de los periodos anteriores (15): %f\n" % C15)
+        f.write("      - Adquisicón o rehabilitación de vivienda habitual (16): %f\n"% C16)
+        f.write("   - Total (17): %f\n" % C17)
+        f.write("   - A deducir (autoliquidación complementaria)\n")
+        f.write("      - Resultado a ingresar anteriores autoliquidaciones (18): %f\n" % C18)
+        f.write("   - Resultado de la autoliquidación (19): %f\n" % C19)
+        
